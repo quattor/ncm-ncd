26336d68ba46a00b56e032cf1250fd3fc15203ec
jouvin <jouvin@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-03-23 11:07:54 +0000

    2011-03-23  Michel Jouvin
    	* Release: 1.3.3
    	- Fix escape() method (not declared as a class method)
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-ncd@2719 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ COMP=ncm-ncd
 
 NAME=$(COMP)
 DESCR=ncd is the NCM client front end for invoking components
-VERSION=1.3.2
+VERSION=1.3.3
 RELEASE=1
 
 AUTHOR=German Cancio <German.Cancio@cern.ch>
@@ -21,4 +21,4 @@ MANSECT=1
 
 TARFILE=ncm-ncd-1.3.0.src.tgz
 PROD=\#
-DATE=01/09/10 07:39
+DATE=23/03/11 12:06
