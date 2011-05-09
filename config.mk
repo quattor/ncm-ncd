125bca2a246dce9b7073b29f6cbab85a375941a0
nwilliams <nwilliams@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-05-09 17:38:08 +0000

    2011-05-09  Nick Williams
    	* Release: 1.4.1
    	- add in some taint checks given that ncm-ncd now is in taint-warn mode
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-ncd@2771 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ COMP=ncm-ncd
 
 NAME=$(COMP)
 DESCR=ncd is the NCM client front end for invoking components
-VERSION=1.4.0
+VERSION=1.4.1
 RELEASE=1
 
 AUTHOR=German Cancio <German.Cancio@cern.ch>
@@ -21,4 +21,4 @@ MANSECT=1
 
 TARFILE=ncm-ncd-1.3.0.src.tgz
 PROD=\#
-DATE=09/05/11 11:48
+DATE=09/05/11 13:37
