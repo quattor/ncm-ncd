105cf62311e7772128154a0abd26d7790ce62c7e
brarda <brarda@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-10-21 09:38:39 +0000

    2011-10-21  Loic Brarda
    	* Release: 1.5.0
    	- Add option to set the syslog facility (defaults to local1)
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-ncd@3003 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -6,8 +6,8 @@ COMP=ncm-ncd
 
 NAME=$(COMP)
 DESCR=ncd is the NCM client front end for invoking components
-VERSION=1.4.1
-RELEASE=2
+VERSION=1.5.0
+RELEASE=1
 
 AUTHOR=German Cancio <German.Cancio@cern.ch>
 MAINTAINER=German Cancio <German.Cancio@cern.ch>
@@ -21,4 +21,4 @@ MANSECT=1
 
 TARFILE=ncm-ncd-1.3.0.src.tgz
 PROD=\#
-DATE=09/05/11 13:37
+DATE=21/10/11 11:37
